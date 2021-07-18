aa0821617028c4db4b31ac3031a6500986fc4353
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-07-18 22:48:17 +0300

    Правит задачу 2, выполняет задачу 8

--- js/task-02.js
+++ js/task-02.js
@@ -4,25 +4,30 @@
 // <ul id="ingredients"></ul>
 // В JS есть массив строк.
 
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+let ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы']
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
 
+const ingredientsEl = document.querySelector('#ingredients')
 
-const ingredientsEl = document.querySelector("#ingredients")
+// ingredientsEl.append(
+//   ...ingredients.map(element => {
+//     const ingredientItemEl = document.createElement('li')
+//     ingredientItemEl.textContent = element
+//     console.log(ingredientItemEl)
+//     return ingredientItemEl
+//   }),
+// )
 
-ingredients.forEach(element => {
-   const ingredientItemEl = document.createElement('li');
-    ingredientItemEl.textContent = element;
-    ingredientsEl.appendChild(ingredientItemEl)
-});
+// ingredients.forEach((element, index, array) => {
+//   const ingredientItemEl = document.createElement('li')
+//   ingredientItemEl.textContent = element
+//   array[index] = ingredientItemEl
+// })
 
-console.log(ingredientsEl);
+ingredients = ingredients.map(element => {
+  const ingredientItemEl = document.createElement('li')
+  ingredientItemEl.textContent = element
+  return ingredientItemEl
+})
 
-
+ingredientsEl.append(...ingredients)
---
+++ js/task-08.js
@@ -0,0 +1,39 @@
+const inputEl = document.querySelector('#controls input')
+
+const btnCreateEl = document.querySelector('[data-action="render"]')
+
+const btnClearEl = document.querySelector('[data-action="destroy"]')
+
+const divContainer = document.querySelector('#boxes')
+
+btnCreateEl.addEventListener('click', () => createBoxes(+inputEl.value))
+btnClearEl.addEventListener('click', clearBoxes)
+
+function clearBoxes() {
+  divContainer.innerText = ''
+}
+
+function createBoxes(amount) {
+  divContainer.innerText = ''
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div')
+    divEl.style.width = 30 + i * 10 + 'px'
+    divEl.style.height = 30 + i * 10 + 'px'
+
+    divEl.innerText = `${i + 1}`
+
+    divContainer.append(divEl)
+    // divEl.setAttribute(width, 30);
+    let red = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+    console.log(red, green, blue)
+
+    divEl.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+
+    console.log(divEl.style.width)
+  }
+}
+
+// btnClearEl.addEventListener('click', clearBoxes);
