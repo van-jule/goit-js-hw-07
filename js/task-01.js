574e0faf09e435fea00fdbe67cd9b0289405b7eb
Yuliia Kharchenko <jkharchenko2008@gmail.com>
2021-07-21 10:11:03 +0300

    обновление дз

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,7 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
 
 const categoriesEl = document.querySelector("#categories")
-// console.log('categoriesEl:', categoriesEl);
-
 const arrayCategoriesEl = categoriesEl.querySelectorAll(".item");
-// console.log('arrayCategoriesEl:', arrayCategoriesEl);
 
  
 function showQuantityOfCategories(){
@@ -14,8 +11,6 @@ function showQuantityOfCategories(){
 }
  
 showQuantityOfCategories();
-
-
 
 
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
@@ -27,7 +22,6 @@ showQuantityOfCategories();
 
 
 const categoryTitlesEl = document.querySelectorAll('.item h2');
-// console.log(categoryTitlesEl);
 
 categoryTitlesEl.forEach(element => {
     console.log(`Категория: ${element.textContent}`);
@@ -35,18 +29,8 @@ categoryTitlesEl.forEach(element => {
 
 
 const sublistEl = document.querySelectorAll('li.item ul');
-// console.log('sublistEl', sublistEl);
 
 sublistEl.forEach(element => {
     console.log(`Количество элементов: ${element.children.length}`);
 });
-
-
-
-// arrayCategoriesEl.forEach(element => {
-//     console.log(`Категория: ${element.firstElementChild.textContent}`);
-//     console.log(`Категория: ${element.lastElementChild.children.length}`);
-
-// });
-
 
--- js/task-04.js
+++ js/task-04.js
@@ -1,22 +1,21 @@
-let spanEl = document.querySelector('#value');
-let counterValue = Number(spanEl.innerText);
+let spanEl = document.querySelector('#value')
+let counterValue = Number(spanEl.innerText)
 
 let btnDecrement = document.querySelector('[data-action="decrement"]')
-console.log(btnDecrement);
+console.log(btnDecrement)
 
 let btnIncrement = document.querySelector('[data-action="increment"]')
-console.log(btnIncrement);
+console.log(btnIncrement)
 
-btnDecrement.addEventListener('click', decrement);
-btnIncrement.addEventListener('click', increment);
+btnDecrement.addEventListener('click', OnClickDecrement)
+btnIncrement.addEventListener('click', OnClickIncrement)
 
-
-function increment() {
-    counterValue += 1;
-    spanEl.innerText = counterValue;
+function OnClickIncrement(evt) {
+  counterValue += 1
+  spanEl.innerText = counterValue
 }
 
-function decrement() {
-    counterValue-=1;
-    spanEl.innerText = counterValue;
+function OnClickDecrement(evt) {
+  counterValue -= 1
+  spanEl.innerText = counterValue
 }
